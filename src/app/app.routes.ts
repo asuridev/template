ecb8c8c6500daac50a841711b2a82d46466259a6
antonio suarez <antonio.suarez@sofka.com.co>
2026-05-12 19:19:49 -0500

    add partnerGuard for route protection and update app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
+import { partnerGuard } from './guards/partner.guard';
 
 export const routes: Routes = [
   {
+    path: 'not-found',
+    loadComponent: () => import('./not-found/pages/not-found.page'),
+  },
+  {
     path: ':partnerId',
+    canActivate: [partnerGuard],
     children: [
       {
         path: 'home',
